15de19ee0c7ed656bb497e097171da43b394feac
Matthew Casperson <matthewcasperson@gmail.com>
2026-02-14 09:23:22 +1000

    Implement spaces fetching functionality and update UI to display spaces

--- dashboards/api.js
+++ dashboards/api.js
@@ -1,9 +1,3 @@
 function getDashboardConfig(callback) {
     chrome.storage.local.get("dashboardConfig", config => callback(config.dashboardConfig));
 }
-
-function getSpaces(callback) {
-    getDashboardConfig(config => {
-
-    })
-}
