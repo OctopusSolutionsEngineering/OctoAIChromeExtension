8872f6a15adfbc5d4a484daf53e2b7c3a91adfb5
Matthew Casperson <matthewcasperson@gmail.com>
2026-02-18 09:27:08 +1000

    Make functions really private

--- dashboards/api.js
+++ dashboards/api.js
@@ -33,92 +33,94 @@ function dashboardSendPrompt(prompt, serverUrl) {
                 state: "Error"
             }
         });
-}
 
-/**
- * Gets the Octopus CSRF token from the browser cookies for the given server URL.
- * @private
- */
-function _dashboardGetOctopusCsrfTokenFromCookie(domain) {
-    return chrome.cookies.getAll({domain: domain})
-        .then(cookies => cookies
-            .filter(cookie => cookie.name.startsWith('Octopus-Csrf-Token'))
-            .map(cookie => cookie.value)
-            .pop())
-}
+    /**
+     * Gets the Octopus CSRF token from the browser cookies for the given server URL.
+     * @private
+     */
+    function _dashboardGetOctopusCsrfTokenFromCookie(domain) {
+        return chrome.cookies.getAll({domain: domain})
+            .then(cookies => cookies
+                .filter(cookie => cookie.name.startsWith('Octopus-Csrf-Token'))
+                .map(cookie => cookie.value)
+                .pop())
+    }
 
-/**
- * Convert the SSE response from the dashboard into a format that can be displayed in the UI.
- * @private
- */
-function _dashboardConvertFromSseResponse(sseResponse) {
-    if (sseResponse.error) {
+    /**
+     * Convert the SSE response from the dashboard into a format that can be displayed in the UI.
+     * @private
+     */
+    function _dashboardConvertFromSseResponse(sseResponse) {
+        if (sseResponse.error) {
+            return {
+                response: sseResponse.error,
+                state: "Error"
+            };
+        }
+
+        if (_dashboardIsActionSseResponse(sseResponse.response)) {
+            return _dashboardConfirmationResponse(sseResponse.response)
+        }
+
         return {
-            response: sseResponse.error,
-            state: "Error"
+            response: _dashboardConvertFromSuccessSseResponse(sseResponse.response),
+            state: "Success"
         };
     }
 
-    if (_dashboardIsActionSseResponse(sseResponse.response)) {
-        return _dashboardConfirmationResponse(sseResponse.response)
+    /**
+     * Check if the SSE response is an action response that requires confirmation.
+     * @private
+     */
+    function _dashboardIsActionSseResponse(sseResponse) {
+        return !!sseResponse
+            .split('\n')
+            .filter(line => line.trim())
+            .filter(line => line.startsWith('data: '))
+            .map(line => JSON.parse(line.replace('data: ', '')))
+            .filter(response => 'type' in response && response.type === 'action')
+            .pop()
     }
 
-    return {
-        response: _dashboardConvertFromSuccessSseResponse(sseResponse.response),
-        state: "Success"
-    };
+    /**
+     * Extract the confirmation message and id from the SSE response.
+     * @private
+     */
+    function _dashboardConfirmationResponse(sseResponse) {
+        return sseResponse
+            .split('\n')
+            .filter(line => line.trim())
+            .filter(line => line.startsWith('data: '))
+            .map(line => JSON.parse(line.replace('data: ', '')))
+            .filter(response => 'type' in response && response.type === 'action')
+            .map(response => {
+                return {
+                    response: response.title + "\n\n" + response.message,
+                    state: "Confirmation",
+                    id: response.confirmation.id
+                }
+            })
+            .pop()
+    }
+
+    /**
+     * Convert a successful SSE response from the dashboard into a format that can be displayed in the UI.
+     * @private
+     */
+    function _dashboardConvertFromSuccessSseResponse(sseResponse) {
+        const responses = sseResponse
+            .split('\n')
+            .filter(line => line.trim())
+            .map(line => JSON.parse(line.replace('data: ', '')));
+
+        const contentResponses = responses
+            .filter(response => 'content' in response.choices[0].delta);
+
+        return contentResponses
+            .map(line => line.choices[0].delta.content.trim())
+            .join('\n');
+    }
 }
 
-/**
- * Check if the SSE response is an action response that requires confirmation.
- * @private
- */
-function _dashboardIsActionSseResponse(sseResponse) {
-    return !!sseResponse
-        .split('\n')
-        .filter(line => line.trim())
-        .filter(line => line.startsWith('data: '))
-        .map(line => JSON.parse(line.replace('data: ', '')))
-        .filter(response => 'type' in response && response.type === 'action')
-        .pop()
-}
 
-/**
- * Extract the confirmation message and id from the SSE response.
- * @private
- */
-function _dashboardConfirmationResponse(sseResponse) {
-    return sseResponse
-        .split('\n')
-        .filter(line => line.trim())
-        .filter(line => line.startsWith('data: '))
-        .map(line => JSON.parse(line.replace('data: ', '')))
-        .filter(response => 'type' in response && response.type === 'action')
-        .map(response => {
-            return {
-                response: response.title + "\n\n" + response.message,
-                state: "Confirmation",
-                id: response.confirmation.id
-            }
-        })
-        .pop()
-}
-
-/**
- * Convert a successful SSE response from the dashboard into a format that can be displayed in the UI.
- * @private
- */
-function _dashboardConvertFromSuccessSseResponse(sseResponse) {
-    const responses = sseResponse
-        .split('\n')
-        .filter(line => line.trim())
-        .map(line => JSON.parse(line.replace('data: ', '')));
-
-    const contentResponses = responses
-        .filter(response => 'content' in response.choices[0].delta);
-
-    return contentResponses
-        .map(line => line.choices[0].delta.content.trim())
-        .join('\n');
-}
 
