6f08a780129a92e045aad74cfa272aff6e665ae2
Matthew Casperson <matthewcasperson@gmail.com>
2026-02-23 10:34:32 +1000

    Add Azure Function and Azure Web App options to dashboard

--- dashboards/easymode/dashboard.js
+++ dashboards/easymode/dashboard.js
@@ -5,6 +5,8 @@ const platformPrompts = {
     awslambda: 'Create an AWS Lambda project called "My AWS Lambda App"',
     scriptstep: 'Create a Script project called "My Script App"',
     bluegreen: 'Create a Blue/Green deployment project called "My Blue Green App"',
+    azurefunction: 'Create an Azure Function project called "My Azure Function App"',
+    azurewebapp: 'Create an Azure Web App project called "My Azure Web App"',
     terraform: 'Create a Terraform project called "My Terraform Infrastructure"',
     tomcat: 'Create a Tomcat project called "My Tomcat WebApp"'
 };
